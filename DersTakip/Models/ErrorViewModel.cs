38248526e372d9f091546e8b15e88d6202202e0b
Irem-ARICI <142163409+Irem-ARICI@users.noreply.github.com>
2024-03-06 02:19:40 +0300

    Add project files.

---
+++ DersTakip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DersTakip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
